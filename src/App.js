3e2b21a70d0e6da3fa02ee095c03dc30778abccd
Pedro Chavez-nolasco <pedrochavez-nolasco@Pedros-MacBook-Pro-2.local>
2022-10-13 12:48:06 -0700

    adding css to home page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Projects from './pages/Projects';
 import Experience from './pages/Experience';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <Route exact path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/experience" element={<Experience />} />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
       </Router>
       <Footer/>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { GitHub, LinkedIn, Email } from '@mui/icons-material';
+import '../styles/Home.css';
 
 
 function Home() {
   return (
     <div className='home'>
       <div className='about'>
-        <h2>Hello my name is Pedro</h2>
-        <div className='skills'>
-          <p>A Software Developer with a passion for learning!</p>
-          <Email />
+        <h2>Hello My Name is Pedro</h2>
+        <div className='prompt'>
+          <p>I am a Software Developer with a passion for learning!</p>
           <LinkedIn />
           <GitHub />
+          <Email />
+
         </div>
       </div>
       <div className='skills'>
-
+        <h1>Skills</h1>
+        <ol className='list'>
+          <li className='item'>
+            <h2>Front-End</h2>
+            <span>ReactJS, CSS, HTML, Jest, MaterialUI</span>
+            <h2>Back-End</h2>
+            <span>MySQL, NodeJS, Express </span>
+            <h2>Languages</h2>
+            <span>Javascript </span>
+          </li>
+        </ol>
       </div>
     </div>
   );
