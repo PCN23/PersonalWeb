bb01d7d3579a917b5f92f154a83a019e01e8a39e
Pedro Chavez-nolasco <pedrochavez-nolasco@Pedros-MacBook-Pro-2.local>
2022-10-17 14:28:35 -0700

    added timeline and is working

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,14 +1,31 @@
+/* eslint-disable react/jsx-key */
 import React from 'react';
-import Timeline from '../components/Timeline';
+import events from '../events.json';
+// import Timeline from '../components/Timeline';
 import 'react-vertical-timeline-component/style.min.css';
-// import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 
 function Experience() {
   return (
-    <div>
-      <Timeline />
-      {/* <VerticalTimeline />
-      <VerticalTimelineElement /> */}
+    <div
+      className='experience'
+      style={{ background: '#333', fontFamily: 'Trebuchet Ms' }}
+    >
+      <h1>Projects Timeline</h1>
+      <VerticalTimeline key={events}>
+        {events.map(event => (
+          <VerticalTimelineElement
+            className='vertical-timeline-element--work'
+            date={event.date}
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          >
+            <h3
+              className='vertical-timeline-element-title'
+              dangerouslySetInnerHTML={{ __html: event.event }}
+            />
+          </VerticalTimelineElement>
+        ))}
+      </VerticalTimeline>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,23 +8,23 @@ function Home() {
     <div className='home'>
       <div className='about'>
         <h2>Hello My Name is Pedro</h2>
-        <p>I am a Software Developer with a passion for learning!</p>
-        <LinkedIn />
-        <GitHub />
-        <Email />
+        <div className='prompt'>
+          <p>I am a Software Developer with a passion for learning!</p>
+          <LinkedIn />
+          <GitHub />
+          <Email />
 
+        </div>
       </div>
       <div className='skills'>
         <h1>Skills</h1>
         <ol className='list'>
-          <li className='item'>
-            <h2>Front-End</h2>
-            <span>ReactJS, CSS, HTML, Jest, MaterialUI</span>
-            <h2>Back-End</h2>
-            <span>MySQL, NodeJS, Express </span>
-            <h2>Languages</h2>
-            <span>Javascript </span>
-          </li>
+          <li>Front-End</li>
+          <span>ReactJS, CSS, HTML, Jest, MaterialUI</span>
+          <li>Back-End</li>
+          <span>MySQL, NodeJS, Express </span>
+          <li>Languages</li>
+          <span>Javascript </span>
         </ol>
       </div>
     </div>
