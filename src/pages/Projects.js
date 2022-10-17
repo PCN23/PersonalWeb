2da83e85d7d6d5020229b807e842f6bec08eacad
Pedro Chavez-nolasco <pedrochavez-nolasco@Pedros-MacBook-Pro-2.local>
2022-10-17 15:28:26 -0700

    adding projects inside of projects with images

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import ProjectItem from '../components/ProjectItem';
+import fish from '../assets/Pacc.png';
+
+import '../styles/Projects.css';
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className='projects'>
+      <h1> My Personal Projects</h1>
+      <div className='projectList'>
+        <ProjectItem name='Angry fish PACC' image={fish} />
+        <ProjectItem />
+      </div>
+    </div>
   );
 }
 
